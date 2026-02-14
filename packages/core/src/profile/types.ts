ae1a701dda2eed326667b0588cc881a78930d230
justinhambleton <justin@hambleton.co>
2026-02-14 07:44:24 -0800

    chore: remove orphaned safety adaptation regex constant

--- packages/core/src/profile/types.ts
+++ packages/core/src/profile/types.ts
@@ -1,7 +1,5 @@
 import type { ContextAdaptation } from "../types.js";
 
-export const SAFETY_ADAPTATION_NAME = /(crisis|emergency|harm|suicid|self[-_ ]?harm)/i;
-
 export type GenericObject = Record<string, unknown>;
 
 export type ContextWithPriority = ContextAdaptation & {
